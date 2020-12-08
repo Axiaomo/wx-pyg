d9a314d1096ed64a36a08d201956daf85c3783f6
syq <756243503@qq.com>
2020-12-08 10:30:24 +0800

    动态渲染综合列表

--- pages/goods_list/index.js
+++ pages/goods_list/index.js
@@ -1,11 +1,12 @@
-// pages/goods_list/index.js
+import {
+  request
+} from "../../request/index.js";
 Page({
   /**
    * 页面的初始数据
    */
   data: {
-    tabs: [
-      {
+    tabs: [{
         id: 0,
         value: '综合',
         isActive: true
@@ -20,25 +21,48 @@ Page({
         value: '价格',
         isActive: false
       }
-    ]
+    ],
+    goodsList:[]
   },
-
+  // 请求数据
+  QueryParams: {
+    query: '',
+    cid: '',
+    pagenum: 1,
+    pagesize: 10
+  },
   /**
    * 生命周期函数--监听页面加载
    */
   onLoad: function (options) {
     console.log(options);
+    this.getGoodsList();
   },
-  // 标题点击事件从子组件传递过来
+  // 获取商品数据
+  async getGoodsList() {
+    const result = await request({
+      url: '/goods/search',
+      data: this.QueryParams
+    })
+    this.setData({"goodsList":result.goods})
+    console.log(result)
+  },
+  // 子父传值                                                                                                      
   handleTabsItemChange(e) {
     // 1.获取呗点击的标题索引
-    const { index } = e.detail;
+    const {
+      index
+    } = e.detail;
     // 2.修改数据源
-    let { tabs } = this.data;
-    tabs.forEach((v, i)=>{
+    let {
+      tabs
+    } = this.data;
+    tabs.forEach((v, i) => {
       i === index ? v.isActive = true : v.isActive = false;
       // 3.重新复制到数据源中
-      this.setData({tabs})
+      this.setData({
+        tabs
+      })
     })
   }
 })
