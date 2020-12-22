58551ca8330fe305515c215108abe785274a3c90
syq <756243503@qq.com>
2020-12-22 14:07:23 +0800

    全局loading加载

--- request/index.js
+++ request/index.js
@@ -1,14 +1,27 @@
+// 同时发送异步请求的次数
+let ajaxTimes = 0;
 export const request = (params) => {
-  const baseUrl="https://api-hmugo-web.itheima.net/api/public/v1";
+  const baseUrl = "https://api-hmugo-web.itheima.net/api/public/v1";
+  ajaxTimes++;
+  wx.showLoading({
+    title: '加载中',
+  })
   return new Promise((resolve, reject) => {
     wx.request({
       ...params,
-      url:baseUrl+params.url,
+      url: baseUrl + params.url,
       success: (result) => {
         resolve(result.data.message)
       },
       fail: (err) => {
         reject(err)
+      },
+      complete: () => {
+        ajaxTimes--;
+        if (ajaxTimes === 0) {
+          wx.hideLoading()
+        }
+
       }
     })
   })
