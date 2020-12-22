11a38c889d7265c77e3930026e3d74e3ae7c7047
syq <756243503@qq.com>
2020-12-22 09:52:28 +0800

    下拉刷新上拉加载

--- pages/goods_list/index.js
+++ pages/goods_list/index.js
@@ -22,7 +22,7 @@ Page({
         isActive: false
       }
     ],
-    goodsList:[]
+    goodsList: []
   },
   // 请求数据
   QueryParams: {
@@ -31,11 +31,14 @@ Page({
     pagenum: 1,
     pagesize: 10
   },
+  // 总页数
+  totalPages: 1,
   /**
    * 生命周期函数--监听页面加载
    */
   onLoad: function (options) {
-    console.log(options);
+    this.QueryParams.cid = options.cid || '';
+    this.QueryParams.query = options.query || '';
     this.getGoodsList();
   },
   // 获取商品数据
@@ -44,8 +47,37 @@ Page({
       url: '/goods/search',
       data: this.QueryParams
     })
-    this.setData({"goodsList":result.goods})
-    console.log(result)
+    // 计算总页数
+    this.totalPages = Math.ceil(result.total / this.QueryParams.pagesize);
+    this.setData({
+      // 合并数据 数组拼接
+      goodsList: [...this.data.goodsList, ...result.goods]
+    })
+    // 关闭下拉刷新的窗口
+    wx.stopPullDownRefresh();
+  },
+  // 页面上滑 滚动条触底事件
+  onReachBottom: function () {
+    if (this.QueryParams.pagenum >= this.totalPages) {
+      wx.showToast({
+        title: '没有数据了',
+      })
+    } else {
+      // 获取目前界面中的个数与总的total对比，如果少，继续加载分页
+      this.QueryParams.pagenum++;
+      this.getGoodsList();
+    }
+
+  },
+
+  // 下拉刷新
+  onPullDownRefresh() {
+    console.log("下拉刷新");
+    this.setData({
+      goodsList: []
+    })
+    this.QueryParams.pagenum = 1;
+    this.getGoodsList();
   },
   // 子父传值                                                                                                      
   handleTabsItemChange(e) {
