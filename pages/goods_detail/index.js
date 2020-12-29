700d60125db28ca968b6ef0e2c6a735a5dc30cab
syq <756243503@qq.com>
2020-12-29 10:12:32 +0800

    商品详情

--- pages/goods_detail/index.js
+++ pages/goods_detail/index.js
@@ -1,66 +1,37 @@
-// pages/goods_detail/index.js
+import {
+  request
+} from "../../request/index.js";
 Page({
 
   /**
    * 页面的初始数据
    */
   data: {
-
+    goodsDetail: {},
   },
 
   /**
    * 生命周期函数--监听页面加载
    */
   onLoad: function (options) {
-
+    const {
+      goods_id
+    } = options;
+    this.getGoodsDetail(goods_id)
   },
-
-  /**
-   * 生命周期函数--监听页面初次渲染完成
-   */
-  onReady: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面显示
-   */
-  onShow: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面隐藏
-   */
-  onHide: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面卸载
-   */
-  onUnload: function () {
-
-  },
-
-  /**
-   * 页面相关事件处理函数--监听用户下拉动作
-   */
-  onPullDownRefresh: function () {
-
-  },
-
-  /**
-   * 页面上拉触底事件的处理函数
-   */
-  onReachBottom: function () {
-
-  },
-
-  /**
-   * 用户点击右上角分享
-   */
-  onShareAppMessage: function () {
-
+  // 初始化获取数据
+  async getGoodsDetail(id) {
+    const result = await request({
+      url: '/goods/detail?goods_id=' + id
+    });
+    this.setData({
+      goodsDetail: {
+        pics: result.pics,
+        goods_price: result.goods_price,
+        goods_name: result.goods_name,
+        // 临时把 .webp 文件名改为 .jpg 做部分机型兼容处理
+        goods_introduce: result.goods_introduce.replace(/\.webp/g, '.jpg')
+      }
+    })
   }
 })
